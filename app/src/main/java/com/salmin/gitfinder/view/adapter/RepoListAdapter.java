aa9955f83135d9fb994f23b6753d3376b958156c
salminnella <salminnella@gmail.com>
2019-04-17 21:37:52 -0700

    list adapter binding set

--- app/src/main/java/com/salmin/gitfinder/view/adapter/RepoListAdapter.java
+++ app/src/main/java/com/salmin/gitfinder/view/adapter/RepoListAdapter.java
@@ -1,8 +1,65 @@
 package com.salmin.gitfinder.view.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.salmin.gitfinder.BR;
+import com.salmin.gitfinder.R;
+import com.salmin.gitfinder.databinding.RepoItemsBinding;
+import com.salmin.gitfinder.models.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class RepoListAdapter extends RecyclerView.Adapter<RepoListAdapter.ViewHolder> {
 
+	private final LayoutInflater inflater;
+	private final List<Repository> data;
 
+	public RepoListAdapter(Context context) {
+		inflater = LayoutInflater.from(context);
+		data = new ArrayList<>();
+	}
+
+	public void setData(List<Repository> data) {
+		if (data == null || data.size() == 0) {
+			return;
+		}
+		this.data.clear();
+		this.data.addAll(data);
+		notifyDataSetChanged();
+	}
+
+	@NonNull
+	@Override
+	public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+		RepoItemsBinding repoBinding = DataBindingUtil.inflate(inflater, R.layout.repo_items, viewGroup, false);
+		return new ViewHolder(repoBinding);
+	}
+
+	@Override
+	public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+		viewHolder.binding.setVariable(BR.repository, data.get(i));
+		viewHolder.binding.setVariable(BR.imageAvatar, data.get(i).owner.avatarUrl);
+	}
+
+	@Override
+	public int getItemCount() {
+		return data.size();
+	}
+
+	class ViewHolder extends RecyclerView.ViewHolder {
+
+		private final RepoItemsBinding binding;
+
+		public ViewHolder(@NonNull RepoItemsBinding binding) {
+			super(binding.getRoot());
+			this.binding = binding;
+		}
+	}
 }
