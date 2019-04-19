8c4b8cea94bef9ed0d5cfc32c548a6b4011376ca
salminnella <salminnella@gmail.com>
2019-04-18 23:57:34 -0700

    updated progress bar visibility

--- app/src/main/java/com/salmin/gitfinder/MainActivity.java
+++ app/src/main/java/com/salmin/gitfinder/MainActivity.java
@@ -2,8 +2,10 @@ package com.salmin.gitfinder;
 
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,10 +33,9 @@ public class MainActivity extends AppCompatActivity {
 //	private MainActivityBinding binding;
 	private RepoListViewModel repoListViewModel;
 	private RepoListAdapter repoListAdapter;
-
 	private EditText searchQuery;
 	private RecyclerView recyclerView;
-	private RepoListAdapter adapter;
+	private ProgressBar progressBar;
 
 
 
@@ -63,6 +64,8 @@ public class MainActivity extends AppCompatActivity {
 	private void initView() {
 		searchQuery = (EditText) findViewById(R.id.search_edit_text_main);
 		recyclerView = (RecyclerView) findViewById(R.id.repo_list_main);
+		progressBar = (ProgressBar) findViewById(R.id.progress_main);
+		progressBar.setVisibility(View.GONE);
 		initAdapter();
 
 	}
