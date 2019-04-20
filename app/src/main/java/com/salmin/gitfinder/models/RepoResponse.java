925a22321274db8ae8cc2f87538789c122f6f458
salminnella <salminnella@gmail.com>
2019-04-20 12:37:21 -0700

    added comparable for sorting of the repo list

--- app/src/main/java/com/salmin/gitfinder/models/RepoResponse.java
+++ app/src/main/java/com/salmin/gitfinder/models/RepoResponse.java
@@ -2,7 +2,7 @@ package com.salmin.gitfinder.models;
 
 import com.google.gson.annotations.SerializedName;
 
-public class RepoResponse {
+public class RepoResponse implements Comparable<RepoResponse>{
 
 	@SerializedName("owner")
 	public RepoResponse.Owner owner;
@@ -19,5 +19,17 @@ public class RepoResponse {
 	public class Owner {
 		@SerializedName("avatar_url")
 		public String avatarUrl;
+	}
+
+	/**
+	 * compares the stars count to sort the returned list of repos from Github api
+	 *
+	 * @param repoResponse RepoResponse
+	 * @return int
+	 */
+	@Override
+	public int compareTo(RepoResponse repoResponse) {
+		int comparedStarCount = Integer.parseInt(repoResponse.stargazersCount);
+		return comparedStarCount - Integer.parseInt(this.stargazersCount);
 	}
 }
